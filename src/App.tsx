fb0926a6d2a60c52fccc93772e3df19dd920182e
Alicia Boyd <26505495+aliciaboyd@users.noreply.github.com>
2023-05-09 23:16:32 -0400

    Adds typescript to project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./scss/custom.scss";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <div className="container bg-gray">
+      <h1 className="text-primary">Bootstrap exercises</h1>
+    </div>
+  );
+}
+
+export default App;
